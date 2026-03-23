48ea29c3a53240a2627434a1c7d723bdbf30002f
flifd <ciaranjbrennan@proton.me>
2026-03-23 12:54:45 +0000

    Created basic main menu

--- app/src/main/java/com/example/pokefinderandroid/MainActivity.kt
+++ app/src/main/java/com/example/pokefinderandroid/MainActivity.kt
@@ -1,36 +1,21 @@
 package com.example.pokefinderandroid
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val tv = TextView(this).apply {
-            textSize = 16f
-            text = "Loading Gen3 data..."
-            setPadding(16, 16, 16, 16)
-        }
+        val cv = R.layout.activity_main
 
-        setContentView(tv)
-
-        Thread {
-            try {
-                val hello = Gen3Bridge.getHelloGen3()
-                val data = Gen3Bridge.getFirstStaticEncounter()
-                runOnUiThread {
-                    tv.text = "$hello\n$data"
-                }
-            } catch (e: Exception) {
-                runOnUiThread {
-                    tv.text = "Native call failed: ${e.message}"
-                }
-            }
-        }.start()
+        setContentView(cv)
     }
+
+//    fun generateMon(view: View?) {
+//        val mon = Gen3Bridge.getFirstStaticEncounter()
+//        val textView = findViewById<View?>(R.id.monview) as TextView
+//        textView.text = mon
+//    }
 }
